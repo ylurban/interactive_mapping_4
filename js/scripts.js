628a1fed138ae2bcdfce94d4007d5a0274d64d78
Yuan Lai <yuanlai@172-16-20-42.DYNAPOOL.NYU.EDU>
2017-03-02 15:57:58 -0500

    update

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}
 }).addTo(map);
 
 
+
 $.getJSON('data/Location.geojson', function(jqueryData) {
 
   L.geoJson(fast_rest,{
@@ -15,29 +16,8 @@ $.getJSON('data/Location.geojson', function(jqueryData) {
     fillOpacity: 0.1,
     weight:0.5,
     onEachFeature: onEachFeature
-  }).addTo(map);
-
-
-//////////////////////////////////////////////////////////
-  //add geojson data that jQuery downloaded and parsed
- 
-
-// L.geoJson(jqueryData, {
-
-//   size:1,
-  
-// onEachFeature: function (feature, layer) {
-//       layer.on('click', function() {
-//          console.log('click!', feature)
-//         $('#sidebar h2').text('Jobs: '+ feature.properties.Jobs + ' Transit: '+ feature.properties.Transit)
-//       })
-//     }
-  
-//    }).addTo(map);
-
-  
+  }).addTo(map);  
 }) // this is the end of the $.getJSON callback
-
 
 
 /////Interactions:
@@ -60,7 +40,6 @@ function highlightFeature(e) {
 
 function resetHighlight(e) {
   var geojson;
-  // ... our listeners
   geojson = L.geoJson(communityDistricts);
   geojson.resetStyle(e.target);
   info.update();
@@ -79,29 +58,24 @@ function onEachFeature(feature, layer) {
 }
 
 
-
-/////////////////////////
+///Add interactive legend;
 var info = L.control();
-
 info.onAdd = function (map) {
     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
     this.update();
     return this._div;
 };
 
-// method that we will use to update the control based on feature properties passed
+///method that we will use to update the control based on feature properties passed
 info.update = function (props) {
-    this._div.innerHTML = '<h4>Health Inspections and Grade</h4>' +  (props ?
+    this._div.innerHTML = '<h4>Health Inspection Grade</h4>' +  (props ?
         '<b>' + props.business + '</b><br />' + props.inspection + ' inspections, ' + props.grade + ' % chance to get A'
-        : 'Hover over a food chain store');
+        : 'Hover over a store, click to zoom in.');
 };
-
 info.addTo(map);
 
-/////////////////////////
 
 var legend = L.control({position: 'bottomright'});
-
 legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
@@ -117,91 +91,10 @@ legend.onAdd = function (map) {
 
     return div;
 };
-
 legend.addTo(map);
 
 ///////////////////////////////////////////////////////
 // Click button to reset back:
-// custom zoom bar control that includes a Zoom Home function
-L.Control.zoomHome = L.Control.extend({
-    options: {
-        position: 'topright',
-        zoomInText: '+',
-        zoomInTitle: 'Zoom in',
-        zoomOutText: '-',
-        zoomOutTitle: 'Zoom out',
-        zoomHomeText: '<i class="fa fa-home" style="line-height:1.65;"></i>',
-        zoomHomeTitle: 'Zoom home'
-    },
-
-    onAdd: function (map) {
-        var controlName = 'gin-control-zoom',
-            container = L.DomUtil.create('div', controlName + ' leaflet-bar'),
-            options = this.options;
-
-        this._zoomInButton = this._createButton(options.zoomInText, options.zoomInTitle,
-        controlName + '-in', container, this._zoomIn);
-        this._zoomHomeButton = this._createButton(options.zoomHomeText, options.zoomHomeTitle,
-        controlName + '-home', container, this._zoomHome);
-        this._zoomOutButton = this._createButton(options.zoomOutText, options.zoomOutTitle,
-        controlName + '-out', container, this._zoomOut);
-
-        this._updateDisabled();
-        map.on('zoomend zoomlevelschange', this._updateDisabled, this);
-
-        return container;
-    },
-
-    onRemove: function (map) {
-        map.off('zoomend zoomlevelschange', this._updateDisabled, this);
-    },
-
-    _zoomIn: function (e) {
-        this._map.zoomIn(e.shiftKey ? 3 : 1);
-    },
-
-    _zoomOut: function (e) {
-        this._map.zoomOut(e.shiftKey ? 3 : 1);
-    },
-
-    _zoomHome: function (e) {
-        map.setView([lat, lng], zoom);
-    },
-
-    _createButton: function (html, title, className, container, fn) {
-        var link = L.DomUtil.create('a', className, container);
-        link.innerHTML = html;
-        link.href = '#';
-        link.title = title;
-
-        L.DomEvent.on(link, 'mousedown dblclick', L.DomEvent.stopPropagation)
-            .on(link, 'click', L.DomEvent.stop)
-            .on(link, 'click', fn, this)
-            .on(link, 'click', this._refocusOnMap, this);
-
-        return link;
-    },
-
-    _updateDisabled: function () {
-        var map = this._map,
-            className = 'leaflet-disabled';
-
-        L.DomUtil.removeClass(this._zoomInButton, className);
-        L.DomUtil.removeClass(this._zoomOutButton, className);
-
-        if (map._zoom === map.getMinZoom()) {
-            L.DomUtil.addClass(this._zoomOutButton, className);
-        }
-        if (map._zoom === map.getMaxZoom()) {
-            L.DomUtil.addClass(this._zoomInButton, className);
-        }
-    }
-});
-// add the new control to the map
-var zoomHome = new L.Control.zoomHome();
-zoomHome.addTo(map);
-
-
 
 
 
